9305ef84970c8495b71b2cfe35855377315e63e1
张晓华 <zhangxh@oceansoft.com.cn>
2020-01-17 10:26:53 +0800

    init

--- src/main/java/com/oceansoft/szga/smp/controller/TestController.java
+++ src/main/java/com/oceansoft/szga/smp/controller/TestController.java
@@ -6,7 +6,6 @@ import com.oceansoft.szga.smp.config.domain.ApiPager;
 import com.oceansoft.szga.smp.mapper.TestMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
